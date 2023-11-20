7e56f801dcac5f3499e7b8ee7bfd07b14c27ebfd
joebrooks198600 <76470923+joebrooks198600@users.noreply.github.com>
2023-11-20 11:58:48 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSumHashMetohdApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = { 2, 7, 11, 15 };
+            int target = 9;
+                        
+            Dictionary<int, int> numIndexMap = new Dictionary<int, int>();
+            
+            for(int i=0; i < myarray.Length; i++) 
+            {
+                int complement = target - myarray[i];
+
+                if (numIndexMap.ContainsKey(complement))
+                {
+                    Console.WriteLine("Get Target two numbers: " + myarray[numIndexMap[complement]] + ", " + myarray[i]);
+                    Console.ReadLine();
+                    return; // 找到目標就結束程式
+                }
+
+                // 將當前數字及其索引添加到Dictionary中
+                if (!numIndexMap.ContainsKey(myarray[i]))
+                {
+                    numIndexMap.Add(myarray[i], i);
+                }
+            }
+
+            Console.WriteLine("Did not get Target two!");
+            Console.ReadLine();
+        }
+    }
+}
